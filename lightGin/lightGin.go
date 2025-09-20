765f0e43fd953fe57576dcbfdb5fd2fa020031f1
alporis <595630751@qq.com>
2025-09-20 17:23:41 +0800

    FEAR:增加分组路由功能

--- lightGin/lightGin.go
+++ lightGin/lightGin.go
@@ -2,8 +2,16 @@ package lightGin
 
 import (
 	"net/http"
+	"log"
 )
 
+//开始进行分组路由的构建
+type RouterGroup struct {
+	prefix string
+	middlewares []HandlerFunc //对中间件的支持
+	parent *RouterGroup
+	engine *Engine
+}
 //请求处理函数的定义
 type HandlerFunc func(*Context)
 
@@ -11,12 +19,44 @@ type HandlerFunc func(*Context)
 //并且再此基础上实现动态路由，那么就需要记录路由表
 type Engine struct {
 	router *router
+	*RouterGroup
+	groups []*RouterGroup //存储所有的路由分组
 }
 
 //构造函数,返回一个Engine实例指针
 func New() *Engine {
-	return &Engine{router: newRouter()}
 
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+//为group再添加新的group，并且存储到engine中
+func (group *RouterGroup) Group(prefix string) *RouterGroup{
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups,newGroup)
+	return newGroup
+}
+
+//通过routerGroup来添加路由，此时会先将前缀加到pattern中，再加入到router中
+func (group *RouterGroup) addRoute(method string,comp string,handler HandlerFunc){
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s",method,pattern)
+	group.engine.router.addRoute(method,pattern,handler)
+}
+
+func (group *RouterGroup) GET(pattern string,handler HandlerFunc){
+	group.addRoute("GET",pattern,handler)
+}
+
+func (group *RouterGroup) POST(pattern string,handler HandlerFunc){
+	group.addRoute("POST",pattern,handler)
 }
 
 func(engine *Engine) addRoute(method string,pattern string,handler HandlerFunc){
--- main.go
+++ main.go
@@ -8,23 +8,34 @@ import (
 
 func main() {
 	r := lightGin.New()
-	r.GET("/", func(c *lightGin.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello lightGin</h1>")
+	r.GET("/index", func(c *lightGin.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *lightGin.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello lightGin</h1>")
+		})
 
-	r.GET("/hello", func(c *lightGin.Context) {
-		// expect /hello?name=lightGinktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *lightGin.Context) {
+			// expect /hello?name=lightGinktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *lightGin.Context) {
+			// expect /hello/lightGinktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *lightGin.Context) {
+			c.JSON(http.StatusOK, lightGin.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *lightGin.Context) {
-		// expect /hello/lightGinktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *lightGin.Context) {
-		c.JSON(http.StatusOK, lightGin.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
